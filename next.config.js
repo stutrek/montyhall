86c267ae4fdeee9dc8bd4557c0bc62b2bc162211
Stu Kabakoff <sakabako@gmail.com>
2021-11-28 01:49:06 -0500

    server side render, use preact

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    webpack(config, { dev, isServer }) {
+        // ${previousConfig...}
+
+        // Replace React with Preact only in client production build
+        if (!dev && !isServer) {
+            Object.assign(config.resolve.alias, {
+                react: 'preact/compat',
+                'react-dom/test-utils': 'preact/test-utils',
+                'react-dom': 'preact/compat',
+            });
+        }
+
+        return config;
+    },
+};
--- pages/[doorCount].tsx
+++ pages/[doorCount].tsx
@@ -4,10 +4,13 @@ import { HomeContainer } from '../containers/home';
 
 export default function HomePage() {
     const router = useRouter();
-    console.log(router);
     const { doorCount } = router.query;
-    console.log(doorCount);
-    const count =
-        parseInt(Array.isArray(doorCount) ? doorCount[0] : doorCount) || 3;
+    const count = parseInt(Array.isArray(doorCount) ? doorCount[0] : doorCount);
     return <HomeContainer doorCount={count} />;
+}
+
+export function getServerSideProps() {
+    return {
+        props: {},
+    };
 }
